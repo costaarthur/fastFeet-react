0b6469c5aeab297ff406df701614fb4e124dc7e5
costaarthur <costaarthur@hotmail.com>
2020-04-09 14:21:33 -0300

    rs unform

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import { Container, Page } from './styles';
 
 import logo from '../../assets/fastfeet-logo.png';
 
 export default function Login() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
   return (
     <Page>
       <Container>
         <img src={logo} alt="fastFeet" />
 
-        <form action="">
+        <Form onSubmit={handleSubmit}>
           <strong>SEU E-MAIL</strong>
-          <input name="email" type="email" placeholder="exemplo@email.com" />
+          <Input name="email" type="email" placeholder="exemplo@email.com" />
           <strong>SUA SENHA</strong>
-          <input name="password" type="password" placeholder="**************" />
+          <Input name="password" type="password" placeholder="**************" />
           <button type="submit">Entrar no sistema</button>
-        </form>
+        </Form>
       </Container>
     </Page>
   );
