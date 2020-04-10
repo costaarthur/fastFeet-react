9ad3ca7911760dd55e92c8a61700414385623921
costaarthur <costaarthur@hotmail.com>
2020-04-09 21:49:05 -0300

    adding reactotron redux sagas actions

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 // import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import { Container, Page } from './styles';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import logo from '../../assets/fastfeet-logo.png';
 
@@ -15,9 +18,12 @@ const schema = Yup.object().shape({
 });
 
 export default function Login() {
-  function handleSubmit(data) {
-    console.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
+
   return (
     <Page>
       <Container>
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '../../../services/history';
+import api from '../../../services/api';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'ffsessions', {
+    email,
+    password,
+  });
+
+  const { token, admin } = response.data;
+
+  yield put(signInSuccess(token, admin));
+
+  history.push('/encomendas');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
