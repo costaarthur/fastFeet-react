f37e13fe72dd47712b699ee35b998fe178a06bd9
costaarthur <costaarthur@hotmail.com>
2020-04-27 10:58:17 -0300

    paint selected page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,51 +10,52 @@ import { Container, Content, PropLink } from './styles';
 import logo from '../../assets/fastfeet-logo.png';
 
 export default function Header() {
-  const [selected, setSelected] = useState('encomendas');
-  const [paintEncomendas, setPaintEncomendas] = useState(true);
-  const [paintEntregadores, setPaintEntregadores] = useState(false);
-  const [paintDestinatarios, setPaintDestinatarios] = useState(false);
-  const [paintProblemas, setPaintProblemas] = useState(false);
+  // const [selected, setSelected] = useState('encomendas');
+  // const [paintEncomendas, setPaintEncomendas] = useState(true);
+  // const [paintEntregadores, setPaintEntregadores] = useState(false);
+  // const [paintDestinatarios, setPaintDestinatarios] = useState(false);
+  // const [paintProblemas, setPaintProblemas] = useState(false);
 
   const { name } = store.getState().admin.profile;
   const dispatch = useDispatch();
+  const [, , , urlAtual] = window.location.href.split('/');
 
-  useEffect(() => {
-    function changeHeaderColor() {
-      if (selected === 'encomendas') {
-        setPaintEncomendas(true);
-        setPaintEntregadores(false);
-        setPaintDestinatarios(false);
-        setPaintProblemas(false);
-      }
+  // useEffect(() => {
+  //   function changeHeaderColor() {
+  //     if (selected === 'encomendas') {
+  //       setPaintEncomendas(true);
+  //       setPaintEntregadores(false);
+  //       setPaintDestinatarios(false);
+  //       setPaintProblemas(false);
+  //     }
 
-      if (selected === 'entregadores') {
-        setPaintEntregadores(true);
-        setPaintEncomendas(false);
-        setPaintDestinatarios(false);
-        setPaintProblemas(false);
-      }
+  //     if (selected === 'entregadores') {
+  //       setPaintEntregadores(true);
+  //       setPaintEncomendas(false);
+  //       setPaintDestinatarios(false);
+  //       setPaintProblemas(false);
+  //     }
 
-      if (selected === 'destinatarios') {
-        setPaintDestinatarios(true);
-        setPaintEncomendas(false);
-        setPaintEntregadores(false);
-        setPaintProblemas(false);
-      }
+  //     if (selected === 'destinatarios') {
+  //       setPaintDestinatarios(true);
+  //       setPaintEncomendas(false);
+  //       setPaintEntregadores(false);
+  //       setPaintProblemas(false);
+  //     }
 
-      if (selected === 'problemas') {
-        setPaintProblemas(true);
-        setPaintEncomendas(false);
-        setPaintEntregadores(false);
-        setPaintDestinatarios(false);
-      }
-    }
-    changeHeaderColor();
-  }, [selected]);
+  //     if (selected === 'problemas') {
+  //       setPaintProblemas(true);
+  //       setPaintEncomendas(false);
+  //       setPaintEntregadores(false);
+  //       setPaintDestinatarios(false);
+  //     }
+  //   }
+  //   changeHeaderColor();
+  // }, [selected]);
 
-  function selectPage(page) {
-    setSelected(page);
-  }
+  // function selectPage(page) {
+  //   setSelected(page);
+  // }
 
   function handleSignOut() {
     dispatch(signOut());
@@ -67,32 +68,33 @@ export default function Header() {
           <img src={logo} alt="FastFeet" />
           <PropLink
             to="/encomendas"
-            onClick={() => {
-              selectPage('encomendas');
-              setPaintEncomendas(false);
-            }}
-            selected={paintEncomendas}
+            // onClick={() => {
+            // selectPage('encomendas');
+            // setPaintEncomendas(false);
+            // }
+            // }
+            selected={urlAtual === 'encomendas'}
           >
             ENCOMENDAS
           </PropLink>
           <PropLink
             to="/entregadores"
-            onClick={() => selectPage('entregadores')}
-            selected={paintEntregadores}
+            // onClick={() => selectPage('entregadores')}
+            selected={urlAtual === 'entregadores'}
           >
             ENTREGADORES
           </PropLink>
           <PropLink
             to="/destinatarios"
-            onClick={() => selectPage('destinatarios')}
-            selected={paintDestinatarios}
+            // onClick={() => selectPage('destinatarios')}
+            selected={urlAtual === 'destinatarios'}
           >
             DESTINATÁRIOS
           </PropLink>
           <PropLink
             to="/problemas"
-            onClick={() => selectPage('problemas')}
-            selected={paintProblemas}
+            // onClick={() => selectPage('problemas')}
+            selected={urlAtual === 'problemas'}
           >
             PROBLEMAS
           </PropLink>
